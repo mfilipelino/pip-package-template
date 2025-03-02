9a1d156e15df5d1ea4b85fbe64e53677e774649f
Marcos Lino <marcos.lino@ssense.com>
2025-03-02 17:44:38 -0500

    Simplify package structure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pip-package-setup",
+    name="pip-package-template",
     version="0.1.0",
     packages=find_packages(),
     description="A minimal Python package installable from GitHub",
